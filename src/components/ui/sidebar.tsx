3dea7c2fad1738552a211e31f4f064e4d04fd6f6
‪okeeke “okeeke” alazmi‬‏ <Mnaweralazmi88@gmail.com>
2025-08-18 17:26:14 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -271,7 +271,7 @@ const SidebarTrigger = React.forwardRef<
       data-sidebar="trigger"
       variant="ghost"
       size="icon"
-      className={cn("h-8 w-8", className)}
+      className={cn(className)}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
