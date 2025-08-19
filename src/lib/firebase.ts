a20d568eb3f0cdb39cf9408b86b9573316c236c5
‪okeeke “okeeke” alazmi‬‏ <Mnaweralazmi88@gmail.com>
2025-08-19 05:53:33 +0000

    هناك مشكلة في تسجل الدخول

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   "projectId": "kuwaity-farm",
@@ -17,5 +18,6 @@ const firebaseConfig = {
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, auth, db };
+export { app, auth, db, storage };
