a986125c5929e7af81864a510b7a5f4265deb09c
‪okeeke “okeeke” alazmi‬‏ <Mnaweralazmi88@gmail.com>
2025-09-04 10:42:13 +0000

    لماذا يظهر بالتطبيق فقط معلومه واحدة من جميع قاعدة البيانات

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,24 +1,26 @@
-
+// src/lib/api/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore, serverTimestamp as firestoreServerTimestamp } from "firebase/firestore";
+import {
+  getFirestore,
+  serverTimestamp as firestoreServerTimestamp,
+} from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  "projectId": "kuwaity-farm",
-  "appId": "1:433160071015:web:2a2434f18a7fdc7bddb1c2",
-  "storageBucket": "kuwaity-farm.appspot.com",
-  "apiKey": "AIzaSyCLB7Dfw5jpDQsVvep-OWVeNv9Hq_nQZSs",
-  "authDomain": "kuwaity-farm.firebaseapp.com",
-  "messagingSenderId": "433160071015"
+  apiKey: "AIzaSyCLB7Dfw5jpDQsVvep-OWVeNv9Hq_nQZSs",
+  authDomain: "kuwaity-farm.firebaseapp.com",
+  projectId: "kuwaity-farm",
+  storageBucket: "kuwaity-farm.appspot.com",
+  messagingSenderId: "433160071015",
+  appId: "1:433160071015:web:2a2434f18a7fdc7bddb1c2",
 };
 
-
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const serverTimestamp = firestoreServerTimestamp;
 
 export { app, auth, db, storage, serverTimestamp };
+export default db;
