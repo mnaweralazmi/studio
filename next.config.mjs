05115e55b40aee6079135dcee3d21c08794c5051
‪okeeke “okeeke” alazmi‬‏ <Mnaweralazmi88@gmail.com>
2025-09-10 11:43:14 +0000

    /

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'standalone' is a good default for server environments.
-  // output: 'export' is for static sites and is NOT compatible with App Hosting SSR.
-};
+const nextConfig = {};
 
 export default nextConfig;
