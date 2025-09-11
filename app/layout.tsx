de17d046eb3471b81b73c7a11040e2b8a7312980
‪okeeke “okeeke” alazmi‬‏ <Mnaweralazmi88@gmail.com>
2025-09-11 00:17:43 +0000

    اضف ذكاء وتطور بشكل التطبيق

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
                 <Link
                   href={item.href}
                   key={item.href}
-                  className="flex flex-col items-center justify-center text-muted-foreground hover:text-primary transition-colors w-full h-full"
+                  className="flex flex-col items-center justify-center text-muted-foreground hover:text-primary transition-colors w-full h-full group"
                 >
                   <div className="p-2 rounded-full border border-transparent group-hover:border-primary transition-colors">
                     <item.icon className="h-7 w-7" />
