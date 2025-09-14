795733286d0d4c9cc0e539968820120c6ebc3cf2
‪okeeke “okeeke” alazmi‬‏ <Mnaweralazmi88@gmail.com>
2025-09-14 18:50:41 +0000

    rules_version = '2';
    service firebase.storage {
      match /b/{bucket}/o {

--- components/views/HomeView.tsx
+++ components/views/HomeView.tsx
@@ -178,7 +178,8 @@ export default function HomeView({ user }: { user: User }) {
 
       if (file) {
         const fileType = file.type.startsWith('image/') ? 'image' : 'video';
-        const filePath = `topics/${user.uid}/${Date.now()}_${file.name}`;
+        // This path MUST match the storage rules structure: /{userId}/{filename}
+        const filePath = `${user.uid}/${Date.now()}_${file.name}`;
         const fileRef = ref(storage, filePath);
         await uploadBytes(fileRef, file);
         const fileUrl = await getDownloadURL(fileRef);
