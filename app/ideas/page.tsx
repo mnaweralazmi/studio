c6501c6aa8fe4da90441d72be1241313ccf482dc
‪okeeke “okeeke” alazmi‬‏ <Mnaweralazmi88@gmail.com>
2025-09-19 23:19:30 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- app/ideas/page.tsx
+++ app/ideas/page.tsx
@@ -16,7 +16,7 @@ import {
 } from 'lucide-react';
 
 import { Toaster } from '@/components/ui/toaster';
-import IdeasView, { Topic } from '@/components/views/IdeasView';
+import IdeasView, { Topic } from '@/components/views/HomeView';
 import {
   Timestamp,
   doc,
